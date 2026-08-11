f5229de18ff520fbc7e74af6833681d39f4bb61a
Ghosuto <clash.raja10@gmail.com>
2026-08-11 19:12:50 +0000

    Launcher3: Add support to disable app close blur
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    (cherry picked from commit de96fe4b80454d078f3839c1e6bd35974b1ec044)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- quickstep/src/com/android/quickstep/util/ScalingWorkspaceRevealAnim.kt
+++ quickstep/src/com/android/quickstep/util/ScalingWorkspaceRevealAnim.kt
@@ -39,6 +39,7 @@ import com.android.launcher3.LauncherAnimUtils.SCALE_INDEX_REVEAL_ANIM
 import com.android.launcher3.LauncherAnimUtils.SCALE_INDEX_WORKSPACE_STATE
 import com.android.launcher3.LauncherAnimUtils.VIEW_ALPHA
 import com.android.launcher3.LauncherAnimUtils.WORKSPACE_SCALE_PROPERTY_FACTORY
+import com.android.launcher3.LauncherPrefs
 import com.android.launcher3.LauncherState
 import com.android.launcher3.R
 import com.android.launcher3.anim.AnimatorListeners
@@ -326,8 +327,12 @@ class ScalingWorkspaceRevealAnim(
         Animations.setOngoingAnimation(launcher.hotseat, animators)
     }
 
+    private fun isAppLaunchBlurEnabled(): Boolean {
+        return LauncherPrefs.get(launcher).get(LauncherPrefs.APP_LAUNCH_BLUR_ENABLED)
+    }
+
     private fun addBlurLayer() {
-        if (!Flags.blurredHomeAnimation()) {
+        if (!isAppLaunchBlurEnabled()) {
             return
         }
         val parent = launcher.dragLayer.viewRootImpl?.surfaceControl ?: return
